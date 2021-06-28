2e1dc57a69082d589ceec421e9f636cea40f752f
조상호 컴퓨터 <teha007@naver.com>
2021-06-29 00:08:21 +0900

    파이썬 CGV 영화 티켓 예매 오픈 알리미 만들기 시작

---
+++ crawling_first.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "http://www.cgv.co.kr//common/showtimes/iframeTheater.aspx?areacode=01&theatercode=0013&date=20210630"
+html = requests.get(url)
+
+# print(html.text)
+
+soup = BeautifulSoup(html.text, "html.parser")
+title_list = soup.select("div.info-movie")
+for i in title_list:
+    print(i.select_one("a > strong").text.strip())
